3413928afb3e447ef95548f8f90f5af1322e1ed2
Vadim Tyupanskiy <tivadim@gmail.com>
2013-07-08 06:49:51 +0000

    changed web.js

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var b = fs.readFileSync('index.html');
+  response.send(b.toString());
 });
 
 var port = process.env.PORT || 5000;
